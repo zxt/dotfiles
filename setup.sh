c3babe723ac06ba68b8b96556e08f9222e19d0c0
Zhao Tang <zxt@zhaotang.ca>
2012-08-30 23:17:30 -0400

    change setup.sh to create symlinks with -n flag
    This makes sure symlinks to directories are not followed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -ns ~/dotfiles/bash_profile ~/.bash_profile
+ln -ns ~/dotfiles/bashrc ~/.bashrc
+ln -ns ~/dotfiles/vim ~/.vim
+ln -ns ~/dotfiles/vimrc ~/.vimrc
+ln -ns ~/dotfiles/gitconfig ~/.gitconfig
