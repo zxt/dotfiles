dea098a59273820b98e14d04ecc97ba116e02f50
Zhao Tang <zxt@zhaotang.ca>
2013-01-05 19:03:07 -0500

    change setup script to backup first, use -f so symlinking doesn't fail if already exist

--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-ln -ns ~/dotfiles/bash_profile ~/.bash_profile
-ln -ns ~/dotfiles/bashrc ~/.bashrc
-ln -ns ~/dotfiles/vim ~/.vim
-ln -ns ~/dotfiles/vimrc ~/.vimrc
-ln -ns ~/dotfiles/gitconfig ~/.gitconfig
-ln -ns ~/dotfiles/inputrc ~/.inputrc
-ln -ns ~/dotfiles/minttyrc ~/.minttyrc
+# backup existing dotfiles before making any changes
+# renames file or dir with .old extension, removes if symlink
+backup() {
+  if [ -L $1 ]; then
+    rm $1
+  elif [ -d $1 || -f $1 ]; then
+    mv $1 $1.old
+  fi
+}
+
+backup ~/.bash_profile
+ln -fs ~/dotfiles/bash_profile ~/.bash_profile
+backup ~/.bashrc
+ln -fs ~/dotfiles/bashrc ~/.bashrc
+backup ~/.vim
+ln -fns ~/dotfiles/vim ~/.vim
+backup ~/.vimrc
+ln -fs ~/dotfiles/vimrc ~/.vimrc
+backup ~/.gitconfig
+ln -fs ~/dotfiles/gitconfig ~/.gitconfig
+backup ~/.inputrc
+ln -fs ~/dotfiles/inputrc ~/.inputrc
+backup ~/.minttyrc
+ln -fs ~/dotfiles/minttyrc ~/.minttyrc
 
 git clone https://github.com/milkbikis/powerline-bash ~/dotfiles/powerline-bash
-ln -s ~/dotfiles/powerline-bash/powerline-bash.py ~/powerline-bash.py
+ln -fs ~/dotfiles/powerline-bash/powerline-bash.py ~/powerline-bash.py
